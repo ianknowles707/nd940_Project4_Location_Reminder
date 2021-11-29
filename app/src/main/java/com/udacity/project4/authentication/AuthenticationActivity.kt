08eef150c5618713402773b766848f0947fd8390
ianknowles707 <72895539+ianknowles707@users.noreply.github.com>
2021-11-29 16:33:32 -0500

    Added Firebase authentication Added custom login screen

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,8 +1,17 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
@@ -10,10 +19,22 @@ import com.udacity.project4.R
  */
 class AuthenticationActivity : AppCompatActivity() {
 
+    companion object {
+        //Define a constant to identify the result of the authentication activity
+        //to ensure we are responding to the correct activity
+        const val SIGN_IN_REQUEST = 1001
+    }
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+
+        //Set listener on the login/signup button to launch Firebase authentication
+        binding.buttonLogin.setOnClickListener {
+            loginOrSignUpWithFirebase()
+        }
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
 
@@ -21,4 +42,50 @@ class AuthenticationActivity : AppCompatActivity() {
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
+    private fun loginOrSignUpWithFirebase() {
+        //Define available sign-in options
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+        //Set custom layout for sign-in screen
+        var customLogin = AuthMethodPickerLayout
+            .Builder(R.layout.authenticate_custom_layout)
+            .setGoogleButtonId(R.id.google_login_provider)
+            .setEmailButtonId(R.id.email_login_provider)
+            .build()
+
+        //Start Firebase authentication. Provide result code to identify the
+        //result of this activity
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setAuthMethodPickerLayout(customLogin)
+                .build(),
+            AuthenticationActivity.SIGN_IN_REQUEST
+        )
+    }
+
+    //Check result of authentication activity - Log response (for now)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == SIGN_IN_REQUEST) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                Log.i(
+                    "Sign-in: ", "OK, user ${
+                        FirebaseAuth.getInstance().currentUser?.displayName
+                    }"
+                )
+            } else {
+                Log.i(
+                    "Sign-in: ", "Unsuccessful, ${response?.error?.errorCode}"
+                )
+            }
+        }
+    }
+
 }
